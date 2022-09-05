67367c319000987c8b553948457ce6ae627b5d70
John <example@gmail.com>
2022-09-05 16:32:57 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 100,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   dynamicBullets: true,
+  // },
+});

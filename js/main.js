3af0ea51f4b89b155e5ddb84a4c099b10647450d
John <example@gmail.com>
2022-09-14 14:48:54 +0600

    testing variables

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
+const obj = {
+  a: '123',
+  b: 'abc'
+};
 
+console.log(obj['a']);
+console.log(obj.b);
 
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  spaceBetween: 100,
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   dynamicBullets: true,
-  // },
-});
+obj.c = true;
+obj.d = {};
+
+console.log(obj.c);
+console.log(obj['d']);
+
+const e = 'myNameis';
+obj[e] = 'Nurike';
+
+console.log(obj);
